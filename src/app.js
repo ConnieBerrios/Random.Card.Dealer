2fa46ae631c563617625648793e6aace48e24f4f
ConnieBerrios <connieberrios@live.com>
2021-07-20 07:00:35 +0000

    ingresojs

--- src/app.js
+++ src/app.js
@@ -2,10 +2,19 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = function(randomcard) {
+  let pinta = ["♠", "♥", "♣", "♦"];
+  let random_pinta = Math.floor(Math.random() * pinta.length);
+  let result = pinta[random_pinta];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let num = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+  let random_num = Math.floor(Math.random() * num.length);
+  let result2 = num[random_num];
+
+  document.getElementById("card").innerHTML = result;
+
+  document.getElementById("numero").innerHTML = result2;
+
+  document.getElementById("card2").innerHTML = result;
+  console.log(randomcard);
 };
